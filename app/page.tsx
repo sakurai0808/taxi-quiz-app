9478047e623ee59d9ce0b9fc4e8352b1c3372115
sakurai0808 <sakuraiwork0808@gmail.com>
2026-02-03 16:42:48 +0900

    型定義をDBに合わせる

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { supabase } from "@/lib/supabase";
 
+
 export default function Home() {
   useEffect(() => {
     const fetchQuestions = async () => {
--- types/quiz.ts
+++ types/quiz.ts
@@ -1,6 +1,8 @@
 export type Question = {
   id: string;
   image_url: string;
-  choices: string[];
   correct_answer: string;
+  choice_2: string;
+  choice_3: string;
+  choice_4: string;
 }
